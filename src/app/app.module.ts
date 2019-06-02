5875497ae1595a26d20e79280b9c6fc877d95916
Rahul Sharma <rahuls91221@gmail.com>
2019-06-02 23:46:36 +0200

    New components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { EntryDetailComponent } from './entry-detail/entry-detail.component';
+import { NewEntryComponent } from './new-entry/new-entry.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    EntryDetailComponent,
+    NewEntryComponent
   ],
   imports: [
     BrowserModule,

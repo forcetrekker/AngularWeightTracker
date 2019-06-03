66d887d858fc42d57e93b5ec73330c520d86bcbe
Rahul Sharma <rahuls91221@gmail.com>
2019-06-03 16:35:31 +0200

    Created login and page not found components, added router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule }   from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { EntryDetailComponent } from './entry-detail/entry-detail.component';
 import { NewEntryComponent } from './new-entry/new-entry.component';
+import { LoginComponent } from './login/login.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const appRoutes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomeComponent },
+  { path: '',   redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     EntryDetailComponent,
-    NewEntryComponent
+    NewEntryComponent,
+    LoginComponent,
+    PageNotFoundComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes
+    ),
     BrowserModule,
     AppRoutingModule,
     FormsModule
